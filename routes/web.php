ebbdf2628791c2993be795f36d84f15009169cef
Jiří Vrba <jiri@vrba.dev>
2021-04-09 08:39:57 +0200

    Add Azure authentication via VSE's tenant

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use RootInc\LaravelAzureMiddleware\Azure;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/login/azure", [Azure::class, "azure"]);
+Route::get("/login/azurecallback", [Azure::class, "azurecallback"]);
