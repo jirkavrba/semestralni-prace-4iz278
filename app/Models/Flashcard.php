5cc1301fe2e8f476ccef3c85ab662e8493bd9372
Jiří Vrba <jiri@vrba.dev>
2021-04-09 10:38:57 +0200

    Add entity relationships between users, collections and flashcards

--- app/Models/Flashcard.php
+++ app/Models/Flashcard.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Flashcard extends Model
 {
     use HasFactory;
+
+    public function collection(): BelongsTo
+    {
+        return $this->belongsTo(FlashcardCollection::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -25,4 +25,9 @@ class User extends Authenticatable
         'email',
         'azure_id',
     ];
+
+    public function collections(): HasMany
+    {
+        return $this->hasMany(FlashcardCollection::class);
+    }
 }
