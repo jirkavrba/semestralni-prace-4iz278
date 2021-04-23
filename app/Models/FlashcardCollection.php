39f1537e65f34c1840e91f60bc900a67c41bc535
Jiří Vrba <jiri@vrba.dev>
2021-04-23 16:34:16 +0200

    Add relationship methods to Eloquent models

--- app/Models/FlashcardCollection.php
+++ app/Models/FlashcardCollection.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
@@ -43,5 +44,10 @@ class FlashcardCollection extends Model
         }
 
         return $query;
+    }
+
+    public function favoritedBy(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, "favorite_collections");
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -31,5 +32,15 @@ class User extends Authenticatable
     public function collections(): HasMany
     {
         return $this->hasMany(FlashcardCollection::class);
+    }
+
+    public function favoriteCollections(): BelongsToMany
+    {
+        return $this->belongsToMany(FlashcardCollection::class, "favorite_collections");
+    }
+
+    public function favoriteFlashcards(): BelongsToMany
+    {
+        return $this->belongsToMany(Flashcard::class, "favorite_flashcards");
     }
 }
