e5a2b1b8c34843b04c74bee96ca97e2eded994d3
Jiří Vrba <jiri@vrba.dev>
2021-04-24 09:11:01 +0200

    Login page design enhancements

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,9 @@
 
         <div class="w-full h-0 border-b border-gray-100 mt-5 mb-10"></div>
 
-        <a href="{{ route("login") }}" class="py-5 px-10 bg-gradient-to-br from-blue-400 to-pink-500 rounded-xl shadow flex flex-row items-center
-        hover:from-blue-500 hover:to-pink-600 hover:shadow-xl transition">
-            <em class="fas fa-key mr-5 text-3xl text-white"></em>
+        <a href="{{ route("login") }}" class="py-5 px-10 bg-blue-500 rounded-xl shadow flex flex-row items-center
+        hover:bg-blue-400 hover:shadow-2xl transition transform hover:-translate-y-1">
+            <em class="fas fa-user-shield mr-5 text-3xl text-white"></em>
             <span class="text-sm font-bold text-white uppercase tracking-wide">
                 Login with VŠE Azure account
             </span>
